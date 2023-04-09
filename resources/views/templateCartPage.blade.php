14acb184357f06a856246280edcbb1459da4023a
John Cthulhu <ivan.dirko@dealinweb.com>
2023-04-09 16:07:06 +0300

    add more product in cart

--- resources/views/templateCartPage.blade.php
+++ resources/views/templateCartPage.blade.php
@@ -2,4 +2,20 @@
 
 @section('content')
     <h1>Its cart page</h1>
+
+   @if($order)
+       <ul class="cart-items">
+       @foreach($order->products as $product)
+           <li class="cart-item">
+               <div>
+                   <a href="{{route('product', [$product->getCategory()->code,$product->code])}}">{{$product->name}}</a>
+                   <form action="{{route('cart-add', $product)}}" method="POST">
+                       <button type="submit">Add more</button>
+                       @csrf
+                   </form>
+               </div>
+           </li>
+       @endforeach
+       </ul>
+   @endif
 @endsection
