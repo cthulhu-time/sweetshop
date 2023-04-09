0670bad838855a45a9b4f2ba2cff0be9627cc532
John Cthulhu <ivan.dirko@dealinweb.com>
2023-04-09 15:18:10 +0300

    Set custom columns for orders table

--- database/migrations/2023_04_09_110507_create_orders_table.php
+++ database/migrations/2023_04_09_110507_create_orders_table.php
@@ -15,6 +15,9 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->tinyInteger('status')->default(0);
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
